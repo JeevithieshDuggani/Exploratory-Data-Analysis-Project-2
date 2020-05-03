3d7b00821d54585f6ae3b97639f09a5482e6654a
JeevithieshDuggani <64452873+JeevithieshDuggani@users.noreply.github.com>
2020-05-03 20:22:04 +0530

    Corrected the spelling of Emission

--- plot1.R
+++ plot1.R
@@ -1,17 +1,19 @@
 ##  Load initialize.R
 source("initialize.R")
-##  Create a list of the total Emmisions in each year
-if (!exists("yearlyEmmisions")) yearlyEmmisions <- tapply(NEI$Emissions, as.factor(NEI$year), sum, simplify = FALSE)
+##  Create a list of the total Emissions in each year
+if (!exists("yearlyEmissions")) yearlyEmissions <- tapply(NEI$Emissions, as.factor(NEI$year), 
+                                                          sum, simplify = FALSE
+                                                   )
 ##  Start the graphic device
 png("plot1.png")
 ##  Plot the graph
-plot(main = "Yearly Emmisions of PM2.5",
-     as.numeric(names(yearlyEmmisions)), yearlyEmmisions, 
-     xlab = "Year", ylab = "Yearly Emmisions",
+plot(main = "Yearly Emissions of PM2.5",
+     as.numeric(names(yearlyEmissions)), yearlyEmissions, 
+     xlab = "Year", ylab = "Yearly Emissions",
      type = "h", lwd = 15, col = "grey", lend = 2
      )
 ##  close the graphic device
 dev.off()
 ##  end
 
-##  The Emmisions drop heavily from 1999 to 2002. From 2002 to 2005 there is a slight decrease and then it drops quite heavily in 2008.
+##  The Emissions drop heavily from 1999 to 2002. From 2002 to 2005 there is a slight decrease and then it drops quite heavily in 2008.
--- plot2.R
+++ plot2.R
@@ -1,19 +1,22 @@
 ##  Load initialize.R
 source("initialize.R")
-##  Create a data frame of the  Emmisions in Baltimore City, Maryland
-if (!exists("baltimoreEmmisions")) baltimoreEmmisions <- NEI[NEI$fips == "24510",]
-##  Create a list of total Emmisions in a year
-if (!exists("yearlyEmmisionsBaltimore")) yearlyEmmisionsBaltimore <- tapply(baltimoreEmmisions$Emissions, baltimoreEmmisions$year, sum)
+##  Create a data frame of the  Emissions in Baltimore City, Maryland
+if (!exists("baltimoreEmissions")) baltimoreEmissions <- NEI[NEI$fips == "24510",]
+##  Create a list of total Emissions in a year
+if (!exists("yearlyEmissionsBaltimore")) yearlyEmissionsBaltimore <- tapply(baltimoreEmissions$Emissions,
+                                                                            baltimoreEmissions$year, 
+                                                                            sum
+                                                                     )
 ##  Start the graphic device
 png("plot2.png")
 ##  Plot the graph
-plot(main = "Yearly Emmisions of PM2.5 in Baltimore City, Maryland",
-     names(yearlyEmmisionsBaltimore), yearlyEmmisionsBaltimore, 
-     xlab = "Year", ylab = "Baltimore City's Yearly Emmisions",
+plot(main = "Yearly Emissions of PM2.5 in Baltimore City, Maryland",
+     names(yearlyEmissionsBaltimore), yearlyEmissionsBaltimore, 
+     xlab = "Year", ylab = "Baltimore City's Yearly Emissions",
      type = "h", lwd = 15, col = "grey", lend = 2
 )
 ##  close the graphic device
 dev.off()
 ##  end
 
-##  The Emmisions drop heavily from 1999 to 2002. From 2002 to 2005 there is a large increase and then it drops to an all time low in 2008.
+##  The Emissions drop heavily from 1999 to 2002. From 2002 to 2005 there is a large increase and then it drops to an all time low in 2008.
